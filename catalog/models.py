105cb395676a476542dafe2407d4c6e355b7ede9
ILINE2006 <teplotom71@gmail.com>
2025-11-17 01:14:30 +0700

    ещё найдены ошибки

--- catalog/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Genre(models.Model):
-    name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
-
-    def __str__(self):
-        return self.name
